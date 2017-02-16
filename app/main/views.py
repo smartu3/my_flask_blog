88afcbbcb02916e1cdfc959daca420ad5891dd4c
zhze <zhze93@qq.com>
2017-02-16 19:12:58 +0800

    merge index/pop post

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,11 @@ import os
 
 @main.route('/')
 def index():
-	return render_template('index.html')
+	page = request.args.get("page",1,type=int)
+	pagination = Post.query.order_by(Post.clicknum.desc()).paginate(page,per_page = \
+		int(15),error_out = False)
+	posts = pagination.items
+	return render_template('index.html',pagination=pagination,posts=posts)
 
 @main.route('/all-posts')
 def posts():
@@ -80,13 +84,7 @@ def post(id):
 	comments=pagination.items
 	return render_template('post.html',posts=[post],form=form,comments=comments,pagination=pagination)
 
-@main.route('/popular-post')
-def popular_post():
-	page = request.args.get("page",1,type=int)
-	pagination = Post.query.order_by(Post.clicknum.desc()).paginate(page,per_page = \
-		int(15),error_out = False)
-	posts = pagination.items
-	return render_template('popular-posts.html',pagination=pagination,posts=posts)
+
 
 @main.route('/edit/<int:id>',methods=['GET','POST'])
 @login_required
